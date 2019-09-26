a74712e392fab435c1596998d8bf7472688eefca
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2019-09-26 11:54:40 +0200

    :green_heart: Fix travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ssh -i ./deploy_key hilfling@$SERVER_IP_ADDRESS '
+ssh -i ./id_rsa hilfling@$SERVER_IP_ADDRESS '
 cd /var/www/hilfling-oauth &&
 git pull &&
-docker-compose -f ../docker-compose.yml -f ../docker-compose.deploy.yml up -d --build
+docker-compose -f ./docker-compose.yml -f ./docker-compose.deploy.yml up -d --build
 '
